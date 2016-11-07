ceefabe3ccbf68d2d521945329533bb610c3cb56
Kara Fulgum <karafulgum@gmail.com>
2016-11-06 18:31:03 -0600

    Create the index template

--- app/routes/index.vue
+++ app/routes/index.vue
@@ -1,18 +1,42 @@
 <template lang="html">
   <div class="">
-
+    <div class="control is-grouped">
+      <div class="nav-left">
+        <h1 class="title">Puppies for Adoption</h1>
+      </div>
+      <div class="nav-right">
+        <router-link class="button is-primary":to="{ name: 'new' }">Add A Pupper</router-link>
+      </div>
+    </div>
+    <table class="table">
+      <thead>
+        <th>Name</th>
+        <th>Age</th>
+        <th></th>
+      </thead>
+      <tbody>
+        <tr v-for="puppy in puppies">
+          <td>{{ puppy.name }}</td>
+          <td>{{ puppy.age }}</td>
+          <td>
+            <router-link :to="{ name: 'detail', params: {id: puppy.id}}">read more</routerlink>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-    };
-  },
+    props: ['puppies'],
 
-  methods: {
+    data() {
+        return {};
+    },
 
-  },
+    methods: {
+
+    },
 };
 </script>
